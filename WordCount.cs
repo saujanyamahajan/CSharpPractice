6d1e7c412ccb214e32fed63e503cb5c2885ed8ba
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-11 22:46:52 +0530

    Csharp practice

---
+++ WordCount.cs
@@ -0,0 +1,28 @@
+﻿// using System;
+// using System.Collections.Generic;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Enter a string: ");
+//         string input = Console.ReadLine() ?? "";
+        
+//         var dict = new Dictionary<char, int>();
+
+//         foreach (char c in input)
+//         {
+//             if (dict.ContainsKey(c))
+//                 dict[c]++;
+//             else
+//                 dict[c] = 1;
+//         }
+
+//         Console.WriteLine("Duplicate characters:");
+//         foreach (var pair in dict)
+//         {
+//             if (pair.Value > 1)
+//                 Console.WriteLine($"{pair.Key} → {pair.Value}");
+//         }
+//     }
+// }
