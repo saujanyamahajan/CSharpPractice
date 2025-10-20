7929b254a5bb59eadbe289b775e16532a068b4c4
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-21 02:00:48 +0530

    Missing number

--- 21StringToInt.cs
+++ 21StringToInt.cs
@@ -1,20 +1,20 @@
-//  String to integer conversion (without int.Parse)
-// Approach :
-// ASCII value of digits start from 48. So for each character , subtract 48 it will give corresponding digit.
-using System;
-using System.Linq;
-using System.Collections.Generic;
+// //  String to integer conversion (without int.Parse)
+// // Approach :
+// // ASCII value of digits start from 48. So for each character , subtract 48 it will give corresponding digit.
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
 
-class HelloWorld
-{
-    public static void Main()
-    {
-        String str = "start";
-        int n=0;
-        for(int i=0;i<str.Length;i++)
-        {
-            n=(n*10)+((int)str[i]-48);
-        }
-        Console.WriteLine(n);
-    }
-}
+// class HelloWorld
+// {
+//     public static void Main()
+//     {
+//         String str = "start";
+//         int n=0;
+//         for(int i=0;i<str.Length;i++)
+//         {
+//             n=(n*10)+((int)str[i]-48);
+//         }
+//         Console.WriteLine(n);
+//     }
+// }
