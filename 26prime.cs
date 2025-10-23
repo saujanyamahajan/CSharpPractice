58bc58bc31b77d2e8152d69772358ead7ff3b525
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-23 22:21:48 +0530

    comment code for q26

--- 26prime.cs
+++ 26prime.cs
@@ -1,29 +1,29 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
 
-class HelloWorld
-{
-    public static void Main()
-    {
-        int flag=1;
-        int a = Convert.ToInt32(Console.ReadLine());
-        if(a<=1)
-        flag=0;
-        else
-        {
-            for(int i=2;i<=Math.Sqrt(a);i++)
-            {
-                if(a%i==0)
-                {
-                flag=0;
-                break;
-                }
-            }
-        }
-        if(flag==0)
-        Console.WriteLine("Non prime");
-        else
-        Console.WriteLine("Prime");
-    }
-}
+// class HelloWorld
+// {
+//     public static void Main()
+//     {
+//         int flag=1;
+//         int a = Convert.ToInt32(Console.ReadLine());
+//         if(a<=1)
+//         flag=0;
+//         else
+//         {
+//             for(int i=2;i<=Math.Sqrt(a);i++)
+//             {
+//                 if(a%i==0)
+//                 {
+//                 flag=0;
+//                 break;
+//                 }
+//             }
+//         }
+//         if(flag==0)
+//         Console.WriteLine("Non prime");
+//         else
+//         Console.WriteLine("Prime");
+//     }
+// }
