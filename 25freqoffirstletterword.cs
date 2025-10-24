5e13dcdb5ea88dfdc3768ad362af3bb1b0193029
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-25 00:00:15 +0530

    changing ques num

--- 25freqoffirstletterword.cs
+++ 25freqoffirstletterword.cs
@@ -1,27 +1,27 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
 
 
-class Program
-{
-    static void Main()
-    {
-        string str="My name is Saujanya Mahajan";
-        string[] arr=str.Split(' ');
-        Dictionary<char,List<string>> dict= new Dictionary<char,List<string>>();
-        foreach (string word in arr)
-        {
-        char firstChar = char.ToUpper(word[0]);
+// class Program
+// {
+//     static void Main()
+//     {
+//         string str="My name is Saujanya Mahajan";
+//         string[] arr=str.Split(' ');
+//         Dictionary<char,List<string>> dict= new Dictionary<char,List<string>>();
+//         foreach (string word in arr)
+//         {
+//         char firstChar = char.ToUpper(word[0]);
      
-        if(!dict.ContainsKey(firstChar))
-                dict[firstChar]=new List<string>();
+//         if(!dict.ContainsKey(firstChar))
+//                 dict[firstChar]=new List<string>();
                 
-        dict[firstChar].Add(word);
-        }
-        foreach(var kvp in dict)
-        {
-            Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
-        }
-    }
-}
+//         dict[firstChar].Add(word);
+//         }
+//         foreach(var kvp in dict)
+//         {
+//             Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+//         }
+//     }
+// }
