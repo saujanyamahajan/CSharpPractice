74c8dba12c38b1adb950f9e5bd69edd447532336
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-24 20:33:00 +0530

    edit ques numbers

--- 24FreqOfEleInString.cs
+++ 24FreqOfEleInString.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
 
-class Program
-{
-    static void Main()
-    {
-        string str= "holaa amigo yo yo ok ok";
-        string[] arr = str.Split(' '); // Split by space
-        Dictionary<string, int> dict = new Dictionary<string, int>();
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (string.IsNullOrWhiteSpace(arr[i]))
-                continue;
-            if (dict.ContainsKey(arr[i]))
-                dict[arr[i]] += 1;
-            else
-                dict.Add(arr[i], 1);
-        }
-        foreach(var kvp in dict)
-        {
-            Console.WriteLine($"{kvp.Key} = {kvp.Value}");
-        }
-    }
-}
+// class Program
+// {
+//     static void Main()
+//     {
+//         string str= "holaa amigo yo yo ok ok";
+//         string[] arr = str.Split(' '); // Split by space
+//         Dictionary<string, int> dict = new Dictionary<string, int>();
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//             if (string.IsNullOrWhiteSpace(arr[i]))
+//                 continue;
+//             if (dict.ContainsKey(arr[i]))
+//                 dict[arr[i]] += 1;
+//             else
+//                 dict.Add(arr[i], 1);
+//         }
+//         foreach(var kvp in dict)
+//         {
+//             Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+//         }
+//     }
+// }
