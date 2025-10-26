000f6d3e5239c639644f2291077a157e1baeaa78
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-26 20:23:02 +0530

    String to integer conversion

--- 22MissingNumber.cs
+++ 22MissingNumber.cs
@@ -1,22 +1,22 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
 
 
 
-class Program
-{
-    public static void Main()
-    {
-        int[] arr= {1,3,4,5};
-        int n=arr.Length+1;
-        int sum=0;
-        int expectedSum=(n*(n+1))/2;
-        for(int i=0;i<arr.Length;i++)
-        {
-            sum+=arr[i];
-        }
-        int res=expectedSum-sum;
-        Console.WriteLine(res);
-    }
-}
+// class Program
+// {
+//     public static void Main()
+//     {
+//         int[] arr= {1,3,4,5};
+//         int n=arr.Length+1;
+//         int sum=0;
+//         int expectedSum=(n*(n+1))/2;
+//         for(int i=0;i<arr.Length;i++)
+//         {
+//             sum+=arr[i];
+//         }
+//         int res=expectedSum-sum;
+//         Console.WriteLine(res);
+//     }
+// }
