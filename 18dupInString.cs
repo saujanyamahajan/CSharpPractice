bc46ba36a8befbd39e89aa25ef71792db6345c45
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-27 23:23:21 +0530

    char count in a string

--- 18dupInString.cs
+++ 18dupInString.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// using System;
+// using System.Collections.Generic;
 
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Enter a string");
-        string input=Console.ReadLine()??"";
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Enter a string");
+//         string input=Console.ReadLine()??"";
 
-        Dictionary<char,int> dict=new Dictionary<char,int>();
-        for(int i=0;i<input.Length;i++)
-        {
-            if(dict.ContainsKey(input[i]))
-                dict[input[i]]++;
-            else
-                dict[input[i]]=1;
-        }
-        Console.WriteLine("Duplicate characters:");
-        foreach(var kvp in dict)
-        {
-            if(kvp.Value>1)
-            Console.WriteLine($"{kvp.Key}={kvp.Value}");
-        }
-    }
-}
+//         Dictionary<char,int> dict=new Dictionary<char,int>();
+//         for(int i=0;i<input.Length;i++)
+//         {
+//             if(dict.ContainsKey(input[i]))
+//                 dict[input[i]]++;
+//             else
+//                 dict[input[i]]=1;
+//         }
+//         Console.WriteLine("Duplicate characters:");
+//         foreach(var kvp in dict)
+//         {
+//             if(kvp.Value>1)
+//             Console.WriteLine($"{kvp.Key}={kvp.Value}");
+//         }
+//     }
+// }
--- obj/Debug/net9.0/Practice.AssemblyInfo.cs
+++ obj/Debug/net9.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b56f937e88768b9fedd50ee5a03d75038a23240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea47a04dbe0b6975b09765196a6e8acfa4d472a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
