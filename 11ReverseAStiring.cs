284f1cfea4382d935b7bd73d46f5ba7676994a0c
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-28 00:03:41 +0530

    reverse a string

--- 11ReverseAStiring.cs
+++ 11ReverseAStiring.cs
@@ -1,25 +1,27 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.Write("Enter a string: ");
-//         string input = Console.ReadLine() ?? "";
-//          Console.WriteLine("Reversed string:");
-//         Char[] result = new char [input.Length];
-//         for (int i = input.Length - 1, j = 0; i >= 0; i--, j++)
-//         {
-//             result[j]=input[i];
-//         }
-//         Console.WriteLine(result);
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine() ?? "";
+         Console.WriteLine("Reversed string:");
+        Char[] result = new char [input.Length];        
+        for(int i=input.Length-1,j=0;i>=0;i--,j++)
+        {
+            result[j]=input[i];
+
+        }
+    
+        Console.WriteLine(result);
 
 //INBIULD FUNCTION
 // string reversed = new string(input.Reverse().ToArray());
 // Console.WriteLine($"Reversed string: {reversed}");
 
-//     }
+    }
 
 
-// }
+}
--- 20isPalindrome.cs
+++ 20isPalindrome.cs
@@ -1,42 +1,42 @@
-using System;
+// using System;
 
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Enter a string: ");
-        string input = Console.ReadLine() ?? "";
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Enter a string: ");
+//         string input = Console.ReadLine() ?? "";
 
 
-        bool result = IsPalindrome(input);
+//         bool result = IsPalindrome(input);
 
-        if (result)
-            Console.WriteLine("The string is a palindrome!");
-        else
-            Console.WriteLine("The string is not a palindrome.");
+//         if (result)
+//             Console.WriteLine("The string is a palindrome!");
+//         else
+//             Console.WriteLine("The string is not a palindrome.");
 
-////RETURNING STRING
-        // char[] result = new char[input.Length];
-        // int left = 0, right = input.Length - 1;
+// ////RETURNING STRING
+//         // char[] result = new char[input.Length];
+//         // int left = 0, right = input.Length - 1;
 
-        // while (right >= 0)
-        // {
-        //     result[left++] = input[right--];
-        // }
+//         // while (right >= 0)
+//         // {
+//         //     result[left++] = input[right--];
+//         // }
 
-        // Console.WriteLine("Reversed string: " + new string(result));
-    }
+//         // Console.WriteLine("Reversed string: " + new string(result));
+//     }
 
-    static bool IsPalindrome(string input)
-    {
-        int left=0,right=input.Length-1;
+//     static bool IsPalindrome(string input)
+//     {
+//         int left=0,right=input.Length-1;
 
-        while(right>left)
-        {
-          if(input[left++]!=input[right--])
-           return false;
-        }
-        return true;
-    }
-}
+//         while(right>left)
+//         {
+//           if(input[left++]!=input[right--])
+//            return false;
+//         }
+//         return true;
+//     }
+// }
 
