f187f29987d4be6724ca01d722130d8982371431
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-29 23:46:01 +0530

    Fibonacci series

---
+++ 29Fibonacci.cs
@@ -0,0 +1,27 @@
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class HelloWorld
+{
+    public static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int first=0,second=1,next=0;
+        for(int i=0;i<a;i++)
+        {
+            if(i<=1)
+            next=i;
+            else
+            {
+                next=first+second;
+                first=second;
+                second=next;
+            }
+            Console.WriteLine(next + " ");
+        }
+    }
+}
+
