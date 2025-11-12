b72cc468a765e92e66ae66e218e6275a52ea06c7
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-12 23:47:38 +0530

    charInString

--- 19CharCountInString.cs
+++ 19CharCountInString.cs
@@ -1,24 +1,24 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.WriteLine("Enter the string");
-//         string input=Console.ReadLine()??"";
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the string");
+        string input=Console.ReadLine()??"";
 
-//         Dictionary<char,int> dict= new Dictionary<char,int>();
-//         for(int i=0;i<input.Length;i++)
-//         {
-//             if(dict.ContainsKey(input[i]))
-//                 dict[input[i]]++;
-//             else
-//                 dict[input[i]]=1;
-//         }
-//         foreach(var kvp in dict)
-//         {
-//             Console.WriteLine($"{kvp.Key}={kvp.Value}");
-//         }
-//     }
-// }
+        Dictionary<char,int> dict= new Dictionary<char,int>();
+        for(int i=0;i<input.Length;i++)
+        {
+            if(dict.ContainsKey(input[i]))
+                dict[input[i]]++;
+            else
+                dict[input[i]]=1;
+        }
+        foreach(var kvp in dict)
+        {
+            Console.WriteLine($"{kvp.Key}={kvp.Value}");
+        }
+    }
+}
